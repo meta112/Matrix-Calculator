760d80cea5e860723a38e5f686607a64eb33245a
meta112 <ryan.cj.zhu@gmail.com>
2023-08-07 22:30:51 -0700

    Add input mode toggle, match left and right input

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 
 
 const operationSelect = document.getElementById("select-operation");
-const inputdiv = document.getElementById("matrix-input");
-const inputright = document.getElementById("input-right");
+const inputDiv = document.getElementById("matrix-input");
+const inputRight = document.getElementById("input-right");
+
+const cellTextLeft = document.getElementById("cell-text-left");
+const matrixACells = document.getElementById("matrix-a-cells");
+const matrixATextbox = document.getElementById("matrix-a-textbox");
+
+const cellTextRight = document.getElementById("cell-text-right");
+const matrixBCells = document.getElementById("matrix-b-cells");
+const matrixBTextbox = document.getElementById("matrix-b-textbox");
 
 operationSelect.addEventListener('change', (event) => {
     console.log("changed smth");
     if (event.target.value == "rref"){
-        inputdiv.style.setProperty("grid-template-columns", "1fr");
+        inputDiv.style.setProperty("grid-template-columns", "1fr");
         console.log('rref');
-        inputright.style.setProperty('display', "none");
+        inputRight.style.setProperty('display', "none");
 
     } else {
-        inputdiv.style.setProperty("grid-template-columns", "repeat(2, 1fr)");
+        inputDiv.style.setProperty("grid-template-columns", "repeat(2, 1fr)");
         console.log('not rref');
-        inputright.style.setProperty('display', "inline-block");
+        inputRight.style.setProperty('display', "block");
+    }
+})
+
+cellTextLeft.addEventListener('change', (event) =>{
+    if (event.target.value == "cells"){
+        matrixACells.style.setProperty("display", "block");
+        matrixATextbox.style.setProperty("display", "none");
+    } else if (event.target.value == "textbox"){
+        matrixACells.style.setProperty("display", "none");
+        matrixATextbox.style.setProperty("display", "block");
+    }
+})
+
+cellTextRight.addEventListener('change', (event) =>{
+    if (event.target.value == "cells"){
+        matrixBCells.style.setProperty("display", "block");
+        matrixBTextbox.style.setProperty("display", "none");
+    } else if (event.target.value == "textbox"){
+        matrixBCells.style.setProperty("display", "none");
+        matrixBTextbox.style.setProperty("display", "block");
     }
 })
